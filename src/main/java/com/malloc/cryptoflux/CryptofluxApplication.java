033aa28ea6950960616f86a4089f1e2b8d06ec24
Malek <malek1mmar@gmail.com>
2025-07-30 23:21:15 +0200

    boot the project

---
+++ src/main/java/com/malloc/cryptoflux/CryptofluxApplication.java
@@ -0,0 +1,13 @@
+package com.malloc.cryptoflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptofluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptofluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malloc/cryptoflux/CryptofluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malloc.cryptoflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptofluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
