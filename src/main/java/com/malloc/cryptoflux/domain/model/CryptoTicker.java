0752b8ba21249d4724bcdc70fae408ed45befb67
Malek <malek1mmar@gmail.com>
2025-08-03 22:11:06 +0200

    update domain to record style

--- src/main/java/com/malloc/cryptoflux/domain/model/CryptoTicker.java
+++ src/main/java/com/malloc/cryptoflux/domain/model/CryptoTicker.java
@@ -1,14 +1,10 @@
 package com.malloc.cryptoflux.domain.model;
 
-public class CryptoTicker {
-  private final String symbol;
-  private final double price;
+import java.math.BigDecimal;
+import java.time.Instant;
 
-  public CryptoTicker(String symbol, double price) {
-    this.symbol = symbol;
-    this.price = price;
-  }
-
-  public String getSymbol() { return symbol; }
-  public double getPrice() { return price; }
-}
+public record CryptoTicker(
+    String symbol,
+    BigDecimal price,
+    Instant timestamp
+) {}
